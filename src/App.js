269ddc2da9944f0756015480a0581748d89f05c0
Faisalnk4045 <foysalnk9095@gmail.com>
2021-10-03 23:57:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, tempora veniam. Reiciendis, enim veritatis placeat harum qui ipsa, eius minus accusamus unde dolorem, quos quas magnam dignissimos repellendus nam voluptatibus.
     </div>
   );
 }
